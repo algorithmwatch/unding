3f9cd31d70f8603f5a2c1185f252684bd78c3986
Johannes Filter <hi@jfilter.de>
2021-11-25 15:54:44 +0100

    Add `IS_EMBED` context to templates

--- goliath/utils/context_processors.py
+++ goliath/utils/context_processors.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 
 
-def settings_context(_request):
+def settings_context(request):
     """Settings available by default to the templates context."""
     # Note: we intentionally do NOT expose the entire settings
     # to prevent accidental leaking of sensitive information
@@ -12,4 +12,5 @@ def settings_context(_request):
         "GOOGLE_VERIFICATION": settings.GOOGLE_VERIFICATION,
         # add site because when sending emails, we don't have access to request
         "current_site": Site.objects.get_current(),
+        "IS_EMBED": request.GET.get("is-embed") is not None,
     }
